b54b42237f5b55c3de06dc10d181f1a42a8d775c
AnyISalIn <anyisalin@gmail.com>
2017-01-21 13:08:52 +0800

    update pycat

--- pycat.py
+++ pycat.py
@@ -4,10 +4,20 @@
 import sys
 from optparse import OptionParser
 
+parser = OptionParser(usage="usage: %prog [options] [filename, ...]",
+                      version="%prog 1.0")
+parser.add_option("-n", "--number",
+                  action="store_true",
+                  dest="number",
+                  default=False,
+                  help="show line numbers")
+options, args = parser.parse_args()
+
 
 def Files(files):
     for file in files:
         yield file
+
 
 def Read(args):
     for file in Files(args):
@@ -18,31 +28,23 @@ def Read(args):
             yield e
 
 
-
 def main():
-    parser = OptionParser(usage="usage: %prog [options] [filename, ...]",
-                          version="%prog 1.0")
-    parser.add_option("-n", "--number",
-                      action="store_true",
-                      dest="number",
-                      default=False,
-                      help="show line numbers")
-    options, args = parser.parse_args()
-
     if len(args) < 1:
         parser.error("wrong number of arguments")
 
     if options.number:
         for file in Read(args):
             if not isinstance(file, list):
-                print('\n', file); continue
+                print('\n', file)
+                continue
             for number, line in enumerate(file):
-                result = str(number+1) + ' ' + line
+                result = str(number + 1) + ' ' + line
                 sys.stdout.write(result)
     else:
         for file in Read(args):
             if not isinstance(file, list):
-                print('\n', file); continue
+                print('\n', file)
+                continue
             for line in file:
                 sys.stdout.write(line)
 
